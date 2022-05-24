ea0c0295fefe3aae40114e8e987e0a7d535f7c88
Alexander Morris <73103701+rushork@users.noreply.github.com>
2022-05-24 12:40:19 +0100

    added pee bottle

--- src/main/java/com/rushork/fart/mod/mixin/MixinAnimalPooper.java
+++ src/main/java/com/rushork/fart/mod/mixin/MixinAnimalPooper.java
@@ -4,18 +4,41 @@ import com.rushork.fart.mod.registry.ModItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(CowEntity.class)
 public abstract class MixinAnimalPooper extends AnimalEntity {
 
-    private int poopingTime = (this.random.nextInt(5)+30)*20;
+    private int poopingTime = (this.random.nextInt(120)+300)*20;
     private int fartingTime = (this.random.nextInt(5)+15)*20;
 
     protected MixinAnimalPooper(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (itemStack.isOf(Items.BUCKET) && !this.isBaby()) {
+            player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0F, 1.0F);
+            ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, Items.MILK_BUCKET.getDefaultStack());
+            player.setStackInHand(hand, itemStack2);
+            return ActionResult.success(this.world.isClient);
+        } else if (itemStack.isOf(Items.GLASS_BOTTLE) && !this.isBaby()) {
+            player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0F, 2.0F);
+            ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, ModItems.PEE_BOTTLE.getDefaultStack());
+            player.setStackInHand(hand, itemStack2);
+            return ActionResult.success(this.world.isClient);
+        }
+        return super.interactMob(player, hand);
     }
 
     public void tickMovement() {
@@ -23,7 +46,7 @@ public abstract class MixinAnimalPooper extends AnimalEntity {
         if (!this.world.isClient && this.isAlive() && --this.poopingTime <= 0) {
             this.playSound(SoundEvents.ENTITY_SLIME_SQUISH, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
             this.dropItem(ModItems.POO_NUGGET);
-            this.poopingTime = (this.random.nextInt(5)+30)*20 ;
+            this.poopingTime = (this.random.nextInt(120)+300)*20 ;
         } else if (!this.world.isClient && this.isAlive() && --this.fartingTime <= 0) {
             this.playSound(SoundEvents.BLOCK_FIRE_EXTINGUISH, 0.5f, 0.1f);
             this.fartingTime = (this.random.nextInt(5)+15)*20;
--- src/main/java/com/rushork/fart/mod/registry/ModItems.java
+++ src/main/java/com/rushork/fart/mod/registry/ModItems.java
@@ -1,14 +1,13 @@
 package com.rushork.fart.mod.registry;
 
+import com.rushork.fart.mod.DrinkableItem;
 import com.rushork.fart.mod.Fartmod;
 import com.rushork.fart.mod.toolmaterials.PooToolMaterial;
 import com.rushork.fart.mod.util.ModLootTableModifiers;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.SwordItem;
+import net.minecraft.item.*;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -20,11 +19,13 @@ public class ModItems {
     public static final Item POO_INGOT = new Item(new Item.Settings()
             .group(ItemGroup.MATERIALS));
     public static final Item POO_SWORD = new SwordItem(PooToolMaterial.INSTANCE, 1, 1F, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final DrinkableItem PEE_BOTTLE = new DrinkableItem(new Item.Settings().group(ItemGroup.BREWING).food(new FoodComponent.Builder().hunger(0).snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*5), 1f).build()));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(Fartmod.MOD_ID, "poo_nugget"), POO_NUGGET);
         Registry.register(Registry.ITEM, new Identifier(Fartmod.MOD_ID, "poo_sword"), POO_SWORD);
         Registry.register(Registry.ITEM, new Identifier(Fartmod.MOD_ID, "poo_ingot"), POO_INGOT);
+        Registry.register(Registry.ITEM, new Identifier(Fartmod.MOD_ID, "pee_bottle"), PEE_BOTTLE);
         ModLootTableModifiers.modifyLootTables();
     }
 }
