b11c8aa43ce9831939209d3ad8957569511b3e41
Alexander Morris <73103701+rushork@users.noreply.github.com>
2022-05-25 11:10:25 +0100

    neatening code

--- src/main/java/com/rushork/fart/mod/registry/ModItems.java
+++ src/main/java/com/rushork/fart/mod/registry/ModItems.java
@@ -7,20 +7,19 @@ import com.rushork.fart.mod.util.ModLootTableModifiers;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.*;
-import net.minecraft.potion.Potion;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item POO_NUGGET = new Item(new Item.Settings()
-            .group(ItemGroup.MISC)
-            .food(new FoodComponent.Builder().hunger(1).saturationModifier(2f).snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20*5), 0.75f).build()));
-    public static final Item POO_INGOT = new Item(new Item.Settings()
-            .group(ItemGroup.MATERIALS));
+    // Items
+    public static final Item POO_NUGGET = new Item(new Item.Settings().group(ItemGroup.MISC).food(new FoodComponent.Builder().hunger(1).saturationModifier(2f).snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20*5), 0.75f).build()));
+    public static final Item POO_INGOT = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final DrinkableItem PEE_BOTTLE = new DrinkableItem(new Item.Settings().group(ItemGroup.BREWING).food(new FoodComponent.Builder().hunger(0).snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*5), 1f).build()));
+
+    // Weapons
     public static final SwordItem POO_SWORD = new SwordItem(PooToolMaterial.INSTANCE, 0, 1f, new Item.Settings().group(ItemGroup.COMBAT));
     public static final ModSword FART_SWORD = new ModSword(PooToolMaterial.INSTANCE, 0, 1f, new Item.Settings().group(ItemGroup.COMBAT));
-    public static final DrinkableItem PEE_BOTTLE = new DrinkableItem(new Item.Settings().group(ItemGroup.BREWING).food(new FoodComponent.Builder().hunger(0).snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*5), 1f).build()));
 
     public static void registerItems() {
         // Items
